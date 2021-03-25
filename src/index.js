0b048971275ae7b1a1e1f262c8db734b5a004ed4
Tyler <tyler@webguyty.com>
2021-03-25 13:19:40 -0700

    materialui installed

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "@fortawesome/fontawesome-free/js/fontawesome";
-import "@fortawesome/fontawesome-free/js/solid";
-import "@fortawesome/fontawesome-free/js/regular";
-import "@fortawesome/fontawesome-free/js/brands";
-import "./main.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+import './main.scss';
 
-import App from "./App";
-import "./scripts/slider";
-import "./scripts/scrollSpy";
-import sendEmail from "./scripts/sendEmail";
-import { showGraph, showData } from "./scripts/graph";
+import App from './App';
+import './scripts/slider';
+import './scripts/scrollSpy';
+import sendEmail from './scripts/sendEmail';
+import { showGraph, showData } from './scripts/graph';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 showGraph();
 showData();
 
 // Send message component
-document.getElementById("form").addEventListener("submit", (e) => sendEmail(e));
+document.getElementById('form').addEventListener('submit', e => sendEmail(e));
